54f567f6c4233f0ab24f84ee9415dc87b2b07913
Omkar Powar <60433142+omkarpowar444@users.noreply.github.com>
2025-05-30 13:10:53 +0530

    Update prod_rag.py

--- prod_rag.py
+++ prod_rag.py
@@ -16,7 +16,7 @@ from langchain.cache import RedisCache
 from langchain.globals import set_llm_cache
 
 
-redis_client=redis.Redis(host="localhost" , port=6379 , db=0)
+redis_client=redis.Redis(host="169.254.122.95" , port=6379 , db=0)
 
 nltk.download('punkt')
 
